aa9f97699edbec2f8b391313a65dfdd4b45579e7
TBlais10 <blaislighting@gmail.com>
2021-11-16 19:35:08 -0500

    API key now hidden

--- src/main/java/com/tblais/personalAPI/controllers/APOD/APODController.java
+++ src/main/java/com/tblais/personalAPI/controllers/APOD/APODController.java
@@ -1,18 +1,23 @@
 package com.tblais.personalAPI.controllers.APOD;
 
 import com.tblais.personalAPI.APOD;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/apod")
 public class APODController {
-    private static final String MY_API_KEY = "hF4GZKXvAM5yV5lroYtw7icPcfp297Q8UJn8xgAd";
+    @Autowired
+    private Environment env;
 
-    @GetMapping ("/apod")
+    @GetMapping ("/")
     public APOD apodInfo (RestTemplate restTemplate) {
-        String URL = "https://api.nasa.gov/planetary/apod?api_key=" + MY_API_KEY;
+        String URL = "https://api.nasa.gov/planetary/apod?api_key=" + env.getProperty("nasa.key");
 
         APOD apod = restTemplate.getForObject(URL, APOD.class);
 
@@ -27,5 +32,10 @@ public class APODController {
     public String postApod(
             @RequestParam (name = "param", required = false) String requestParm) {
         return "POST com.tblais.personalAPI.controllers.APOD";
+    }
+
+    @GetMapping("/env")
+    public String envTest(){
+        return env.getProperty("nasa.key");
     }
 }
